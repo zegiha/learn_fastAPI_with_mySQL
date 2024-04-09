43e8117508432c0986b4a920f19336b352a180a5
zegiha <LeeSeoYul070814@gmail.com>
2024-04-09 17:50:36 +0900

    at 13:35

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, HTTPException, Depends, status
+from pydantic import BaseModel
+from typing import Annotated
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+models.Base.metadata.create_all(bind = engine)
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    user_id: int
+
+class UserBase(BaseModel):
+    username: str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
